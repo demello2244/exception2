b14d24017366bafe4cb116b4dc4fef05369dbdc0
Denise Mello <denisemello.zero@gmail.com>
2021-01-01 23:02:52 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		String holder = sc.nextLine();
+		sc.nextLine();
+		System.out.print("Initial balance: ");
+		Double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		Double withdrawLimit = sc.nextDouble();
+		
+		Account ac = new Account(number, holder, balance, withdrawLimit);
+			
+		try {	
+			System.out.println();
+			System.out.print("Enter amount for withdraw: ");
+			Double amount= sc.nextDouble();
+		
+		
+			ac.withdraw(amount);		
+			System.out.printf("New Balance: %.2f", ac.getBalance());
+			
+		}
+		catch (DomainException e) {
+			System.out.println("Withdraw error:"+ e.getMessage());
+		}
+		
+	}
+
+}
